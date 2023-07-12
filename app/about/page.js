5a84f4172d0cd792284f94af52488a58358177b5
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-07-12 12:45:20 -0400

    Begain building imdb clone using next js

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+export default function page(){
+    return(
+        <main className="p-8">
+          <h1 className="p-8">about</h1>
+          <p className="mt-4 text-zinc-400">i forgot how to do lorem </p>
+
+        </main>
+    )
+
+
+}
